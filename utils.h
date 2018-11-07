83ee8b4f02b158e76ceba131a8ed8f83e8fe3a0c
luoyefeisong <xy123858>
2018-11-07 18:04:40 +0800

    peer-channel update

---
+++ utils.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright 2011 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef SIGNALING_SERVER_UTILS_H_
+#define SIGNALING_SERVER_UTILS_H_
+
+
+#ifndef ARRAYSIZE
+#define ARRAYSIZE(x) (sizeof(x) / sizeof(x[0]))
+#endif
+
+#endif  // SIGNALING_SERVER_UTILS_H_
